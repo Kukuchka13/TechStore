03dca06db05cd003f57ce0e141eb7cf2270aa375
Kukuchka13 <agustinkukuchka@gmail.com>
2026-05-29 20:57:41 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -119,26 +119,7 @@ class PCEscritorio extends Producto {
 // ── Catálogo de productos ────────────────────────────────────
 // Array global — definido fuera de cualquier función o clase
 const catalogo = [
-  new Producto(
-    'MacBook Air M2', 'Apple', 2100000, 4,
-    'https://images.unsplash.com/photo-1541807084-5c52b6b3adef?w=400'
-  ),
-  new Producto(
-    'iPhone 15', 'Apple', 1650000, 12,
-    'https://images.unsplash.com/photo-1695048133142-1a20484d2569?w=400'
-  ),
-  new Producto(
-    'Sony WH-1000XM5', 'Sony', 420000, 0,
-    'https://images.unsplash.com/photo-1618366712010-f4ae9c647dcb?w=400'
-  ),
-  new Producto(
-    'LG UltraGear 27GP850', 'LG', 480000, 6,
-    'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=400'
-  ),
-  new Producto(
-    'PC Gamer Entry Level', 'Armada', 1200000, 5,
-    'https://images.unsplash.com/photo-1587202372634-32705e3bf49c?w=400'
-  ),
+  
   new Notebook(
     'Dell XPS 13', 'Dell', 1800000, 7,
     'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=400',
